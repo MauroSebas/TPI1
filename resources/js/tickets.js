b6aab027aa4653e2166278d5cb4281b3c56fc052
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2023-11-04 00:28:37 -0300

    agregando la tarjeta de oradores desde js

---
+++ resources/js/tickets.js
@@ -0,0 +1,12 @@
+//Creo las tarjetas con los descuentos
+
+//Capturo el elemento section
+const tickets = document.getElementById("compraTicket");
+console.log(tickets);
+
+//creo un div hijo de section
+nuevoDiv = document.createElement("div");
+
+
+
+tickets.appendChild(nuevoDiv);
