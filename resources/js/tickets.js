7b4ffd29d18a9242064fd6bbff4157c925d8d44b
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2023-11-06 19:41:28 -0300

    agregado de seccion de compra de tickets

--- resources/js/tickets.js
+++ resources/js/tickets.js
@@ -1,12 +1,39 @@
-//Creo las tarjetas con los descuentos
+// Valor del ticket
+const ticket = 200;
 
-//Capturo el elemento section
-const tickets = document.getElementById("compraTicket");
-console.log(tickets);
+// definir los descuentos
 
-//creo un div hijo de section
-nuevoDiv = document.createElement("div");
+let descEstudiante = 0.80;
+let descTrainee = 0.50;
+let descJunior = 0.15;
 
+// Capturo los elementos
 
+const formulario = document.getElementById('formulario');
+const categoria = document.getElementById('categoria');
+const cantidad = document.getElementById('cantidad');
+const totalPagar = document.getElementById('totalAPagar');
+const botonResumen = document.getElementById('resumen');
+const botonBorrar = document.getElementById('borrar')
 
-tickets.appendChild(nuevoDiv);
+function calcularPago(){
+    let total = cantidad.value * ticket;
+
+    switch (categoria.value) {
+        case "estudiante":
+            total = total - (total * descEstudiante);
+            break;
+        
+        default:
+            break;
+    }
+
+    totalPagar.textContent = "Total a Pagar: " + total;
+
+}
+
+botonResumen.addEventListener('click',(e)=>{
+    e.preventDefault() //Evito que guarde el valor por default y calcule el pago. 
+    calcularPago()
+});
+
